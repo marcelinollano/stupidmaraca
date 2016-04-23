678c8796c75aa6bd3d5c1fbe6b0a3881a28c1745
Fernando Blat <ferblape@gmail.com>
2016-04-23 14:25:32 +0200

    Fine tunning

--- shake.js
+++ shake.js
@@ -1,52 +1,60 @@
 var t1 = new Date().getTime();
+var mainLoopID;
 
 function play(fileName){
   var audio = new Audio('/assets/' + fileName + '.mp3');
   audio.play();
 }
 
+function activateMainLoop(){
+  mainLoopID = setInterval(function () {
+    window.document.body.style.backgroundColor = 'white';
+    play('shake-4');
+  }, 500);
+}
+
 function ping() {
   var t2 = new Date().getTime();
-  var value = t2 - t1
+  var value = t2 - t1;
 
   if (value > 400) {
     window.document.body.style.backgroundColor = 'white';
+    play('shake-4');
+    activateMainLoop();
+  }
+
+  if (value > 150 && value < 400)  {
+    window.clearTimeout(mainLoopID);
+    window.document.body.style.backgroundColor = 'yellow';
     play('shake-1');
   }
-  if (value > 150 && value < 400)  {
-    window.document.body.style.backgroundColor = 'yellow';
-    play('shake-4');
-  }
+
   if (value < 150) { window.document.body.style.backgroundColor = 'red'; }
 
   t1 = t2;
 };
 
+activateMainLoop();
+
 if (typeof window.DeviceMotionEvent != 'undefined') {
-    var sensitivity = 30;
-    var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
+  var sensitivity = 30;
+  var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
 
-    window.addEventListener('devicemotion', function (e) {
-        x1 = e.accelerationIncludingGravity.x;
-        y1 = e.accelerationIncludingGravity.y;
-        z1 = e.accelerationIncludingGravity.z;
-    }, false);
+  window.addEventListener('devicemotion', function (e) {
+    x1 = e.accelerationIncludingGravity.x;
+    y1 = e.accelerationIncludingGravity.y;
+    z1 = e.accelerationIncludingGravity.z;
+  }, false);
 
-    setInterval(function () {
-      var change = Math.abs(x1-x2+y1-y2+z1-z2);
+  setInterval(function () {
+    var change = Math.abs(x1-x2+y1-y2+z1-z2);
 
-      if (change > sensitivity) {
-        ping();
-      }
+    if (change > sensitivity) {
+      ping();
+    }
 
-      x2 = x1;
-      y2 = y1;
-      z2 = z1;
-    }, 40);
-
-    setInterval(function () {
-      window.document.body.style.backgroundColor = 'white';
-      play('shake-1');
-    }, 1000);
-
+    x2 = x1;
+    y2 = y1;
+    z2 = z1;
+  }, 40);
 };
