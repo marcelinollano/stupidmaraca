744a092a2e56983437882a8b488d7534c9072cb8
Marcelino Llano <hello@marcelinollano.com>
2016-04-27 01:30:20 +0200

    Add some video, update the README

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-### 👉 [stupidmaraca.lol](http://stupidmaraca.lol)
+# Stupid Maraca
 
-![Screen Shot](assets/demo.png)
+A maraca for your mobile phone, only in reverse.
+
+This was a project made for the [StupidHackathon 2016](http://www.stupidhackathon.es). Check the project at [stupidmaraca.lol](http://stupidmaraca.lol).
+
+## Demo
+
+You can't grasp this high-concept? [Here is a video](assets/demo.mp4)
+
+![Demo](assets/demo.gif)
+
+## Setup
+
+1. Visit [stupidmaraca.lol](http://stupidmaraca.lol) from your mobile phone.
+2. Add to your home screen: `Status Bar > Add to Home Screen`.
+3. Disable silence and turn the volume up!
+4. Tap the icon of `Stupid Maraca` to launch it.
+4. Chk chk chk, shake the device!
+
+## FAQ
+
+**Q: Who made this?**  
+**A:** It was [@denegro](https://twitter.com/denegro)'s original idea and he also did the cute graphics. [@ferblape](https://twitter.com/ferblape) and [@marcelinollano](https://twitter.com/marcelinollano) implemented the thing, mostly copying from Stack Overflow. [@kekoponte](https://twitter.com/kekoponte) and [@coquedesigns](https://twitter.com/coquedesigns) did testing and emotional support and.
+
+**Q: My maraca does not sound, why?**  
+**A:** You must add it to your home screen (see step 2 in the `Setup` section). Mobile browsers limit the access to the features of the device. The audio API only works if the webapp is installed on the home screen.
+
+**Q: It this great product free?**  
+**A:** Yep, but we may add some in-app purchases to unlock more maracas.
---
+++ assets/script.js
@@ -0,0 +1,78 @@
+var mainLoopID;
+var audioCache = {};
+var $maraca = $('#maraca');
+var colors = ['#ff6000' , '#a951ec' , '#c9fe3e' , '#200274' , '#6dedf7'];
+var t1 = new Date().getTime();
+
+ion.sound({
+  sounds: [{name: "shake"}],
+  volume: 0.5,
+  path: "/assets/",
+  preload: true
+});
+
+setInterval(function () {
+  if(mainLoopID === 0){
+    activateMainLoop();
+  }
+}, 700);
+
+function play(fileName){
+  if(audioCache[fileName] === undefined){
+    audioCache[fileName] = new Audio(fileName + '.mp3');
+  }
+  audioCache[fileName].play();
+}
+
+function activateMainLoop(){
+  $('#splash').hide();
+  $maraca.removeClass("shake-stop");
+  $maraca.addClass("shake-slow");
+  mainLoopID = setInterval(function () {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    window.document.body.style.backgroundColor = color;
+    ion.sound.play("shake");
+  }, 200);
+}
+
+function ping() {
+  var t2 = new Date().getTime();
+  var value = t2 - t1;
+  $maraca.removeClass("shake-slow");
+  $maraca.addClass("shake-stop");
+  window.clearTimeout(mainLoopID);
+  mainLoopID = 0;
+  t1 = t2;
+};
+
+if (typeof window.DeviceMotionEvent != 'undefined') {
+  var sensitivity = 30;
+  var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
+
+  window.addEventListener('devicemotion', function (e) {
+    x1 = e.accelerationIncludingGravity.x;
+    y1 = e.accelerationIncludingGravity.y;
+    z1 = e.accelerationIncludingGravity.z;
+  }, false);
+
+  setInterval(function () {
+    var change = Math.abs(x1-x2+y1-y2+z1-z2);
+
+    if (change > sensitivity) {
+      ping();
+    }
+
+    x2 = x1;
+    y2 = y1;
+    z2 = z1;
+  }, 40);
+};
+
+if ("ontouchstart" in window) {
+  $('#browser-desktop').css('display', 'none'); 
+};
+
+if (("standalone" in window.navigator) && window.navigator.standalone){
+  $('#browser-desktop').css('display', 'none'); 
+  $('#browser-mobile').css('display', 'none'); 
+};
